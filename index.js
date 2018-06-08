fb6755ecfd758641673f0d3e3a5fbb2ce1d2c5a0
Daniel García Aubert <danielgarciaaubert@gmail.com>
2018-06-08 14:31:27 +0200

    Fix linter issues

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 const execSync = require('child_process').execSync;
 const host = execSync('hostname -f').toString('utf8').replace(/\n/g, '');
-const reverse = host.split('.').reverse().join('.');
 
 module.exports = function () {
     return host;
-}
+};
 
 module.exports.reverse = function () {
     return host.split('.').reverse().join('.');
-}
+};
 
 module.exports.hostname = function () {
-    return host.split('.').shift()
-}
+    return host.split('.').shift();
+};
--- test.js
+++ test.js
@@ -3,19 +3,19 @@ const assert = require('assert');
 
 describe('fqdn-sync', function () {
     it('should get fully qualified domain name', function () {
-        assert.ok(fqdn())
+        assert.ok(fqdn());
     });
 
     it('should get fully qualified domain name reversed', function () {
         const hostname = fqdn();
         const reverseHostname = hostname.split('.').reverse().join('.');
 
-        assert.equal(reverseHostname, fqdn.reverse())
+        assert.equal(reverseHostname, fqdn.reverse());
     });
 
     it('should get hostname', function () {
-        const hostname = fqdn().split('.').pop();
+        const hostname = fqdn().split('.').shift();
 
-        assert.equal(hostname, fqdn.hostname())
+        assert.equal(hostname, fqdn.hostname());
     });
 });
